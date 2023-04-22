bccf4c21a99c3ae9d347a25b3ad0d5c3b8778552
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-04-21 23:00:10 -0400

    requerimirnto 4
    actualización a modal junto con elección de familiar o personal de salud

--- pages/publico/usuarionuevo.js
+++ pages/publico/usuarionuevo.js
@@ -1,72 +1,51 @@
-// Arreglo para almacenar los usuarios creados
-let users = [];
+// Creación de usuario
+document.getElementById("crearUsuarioForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evitar el envío del formulario
 
-// Función para crear un nuevo usuario
-function createUser() {
-    const name = document.getElementById('name').value;
-    const role = document.getElementById('role').value;
-    const email = document.getElementById('email').value;
+    // Obtener los valores de los campos del formulario
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var rol = document.getElementById("rol").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var confirmarContrasena = document.getElementById("confirmarContrasena").value;
 
-    // Validación de campos vacíos
-    if (!name || !role || !email) {
-        alert('Por favor complete todos los campos');
+    // Validar que las contraseñas coincidan
+    if (contrasena !== confirmarContrasena) {
+        alert("Las contraseñas no coinciden. Por favor, intenta de nuevo.");
         return;
     }
 
-    // Creación de objeto de usuario
-    const user = {
-        name: name,
-        role: role,
-        email: email
+    // Crear un objeto de usuario con los valores del formulario
+    var usuario = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo,
+        rol: rol,
+        contrasena: contrasena
     };
 
-    // Agregar usuario al arreglo y actualizar la tabla
-    users.push(user);
-    updateTable();
+    // Agregar el usuario a la lista de usuarios
+    var userTableBody = document.getElementById("userTable").getElementsByTagName("tbody")[0];
+    var row = userTableBody.insertRow();
+    var nombreCell = row.insertCell();
+    var rolCell = row.insertCell();
+    var correoCell = row.insertCell();
+    var accionesCell = row.insertCell();
+    nombreCell.textContent = usuario.nombre + " " + usuario.apellido;
+    rolCell.textContent = usuario.rol;
+    correoCell.textContent = usuario.correo;
+    accionesCell.innerHTML = "<button class='btn btn-danger' onclick='eliminarUsuario(this)'>Eliminar</button>";
 
-    // Limpiar campos del formulario
-    document.getElementById('name').value = '';
-    document.getElementById('role').value = 'familiar';
-    document.getElementById('email').value = '';
-}
+    // Cerrar el modal
+    $('#crearUsuarioModal').modal('hide');
 
-// Función para actualizar la tabla de usuarios
-function updateTable() {
-    const tableBody = document.getElementById('userTable').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';
-
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        const row = tableBody.insertRow();
-
-        const nameCell = row.insertCell();
-        nameCell.textContent = user.name;
-
-        const roleCell = row.insertCell();
-        roleCell.textContent = user.role;
-
-        const emailCell = row.insertCell();
-        emailCell.textContent = user.email;
-
-        const actionsCell = row.insertCell();
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Eliminar';
-        deleteButton.className = 'btn btn-danger btn-sm';
-        deleteButton.addEventListener('click', function() {
-            deleteUser(i);
-        });
-        actionsCell.appendChild(deleteButton);
-    }
-}
-
-// Función para eliminar un usuario
-function deleteUser(index) {
-    users.splice(index, 1);
-    updateTable();
-}
-
-// Asignar evento submit al formulario
-document.getElementById('userForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    createUser();
+    // Limpiar el formulario
+    document.getElementById('crearUsuarioForm').reset();
 });
+
+// Función para eliminar un usuario de la lista
+function eliminarUsuario(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
