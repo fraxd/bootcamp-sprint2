da30552f60a8dffc6b35b22c62be5ed682dce1aa
chesumadre <rafael.jarapro@gmail.com>
2023-04-25 18:17:21 -0400

    a

--- JS/calendario.js
+++ JS/calendario.js
@@ -24,7 +24,7 @@ window.addEventListener('load', function () {
 })
 window.crearCalendario = function() {
 let cita = document.querySelector("#ol")
-for (let i = -4; i < 32; i++) {
+for (let i = -4; i < 31; i++) {
 let dia = document.createElement("li")
 let postit = document.createElement("p")
 dia.setAttribute("id", "dia"+i)
@@ -38,16 +38,15 @@ postit.innerText= i;
 }
 }
 window.llenarCalendario=function(){
-    for (let i = 0; i < 31; i++) {
+    for (let i = 1; i < 31; i++) {
+        let card = document.querySelector("#dia"+i)
+            card.innerHTML="<p>"+i+"</p>"
         for (let e = 0; e < window.usuarios.length ; e++){
-            
             let entrada = window.usuarios[e];
             day= entrada.fecha.charAt(8) + entrada.fecha.charAt(9);
             month=entrada.fecha.charAt(5) + entrada.fecha.charAt(6);
             year= entrada.fecha.charAt(0) + entrada.fecha.charAt(1)+ entrada.fecha.charAt(2) + entrada.fecha.charAt(3);
             if(day==i && month==04 && year==2023){
-                let card = document.querySelector("#dia"+i)
-                card.innerText=("")
                 let d= document.createElement("div");
                 card.appendChild(d);
                 d.innerText=entrada.nombrePaciente;
